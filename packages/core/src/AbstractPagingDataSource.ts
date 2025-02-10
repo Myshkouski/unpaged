05be10f449fcef636dfc0d3c1c72460e84f60062
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2025-02-10 18:54:05 +0300

    fix: keys refresh

--- packages/core/src/AbstractPagingDataSource.ts
+++ packages/core/src/AbstractPagingDataSource.ts
@@ -10,7 +10,7 @@ implements PagingDataSource<TKey, TData, TMetadata> {
   abstract get pages(): PagesMap<TKey, TData, TMetadata>
   abstract set pages(value)
 
-  private promise: Promise<PagesMap<TKey, TData, TMetadata>> = Promise.resolve(new Map())
+  private promise: Promise<[TKey, Page<TData, TMetadata>][]> = Promise.resolve([])
 
   abstract loadPage(key: TKey): Promise<TData>
   abstract getPageMetadata(key: TKey, data: TData): TMetadata
@@ -43,21 +43,29 @@ implements PagingDataSource<TKey, TData, TMetadata> {
     return page
   }
 
-  private async tryLoad(keys: TKey[], skipCache: boolean = false) {
+  private async tryLoad(keys: TKey[], options: { skipCache: boolean, clearState: boolean }) {
     await this.promise
 
     this.isPending = true
 
     try {
       const promises = keys.map(key => {
-        return Promise.resolve(
-          true === skipCache ? this.tryLoadData(key) : this.tryGetCachedOrLoad(key)
-        )
+        return true === options.skipCache
+          ? this.tryLoadData(key)
+          : this.tryGetCachedOrLoad(key)
       })
 
-      this.promise = Promise.all(promises).then(entries => new Map(entries))
+      this.promise = Promise.all(promises)
 
-      this.pages = await this.promise
+      const newEntries = await this.promise
+
+      if (options.clearState) {
+        this.pages.clear()
+      }
+      
+      for (const [key, page] of newEntries) {
+        this.pages.set(key, page)
+      }
     } catch (e) {
       //
     }
@@ -66,13 +74,19 @@ implements PagingDataSource<TKey, TData, TMetadata> {
   }
 
   async load(keys: TKey[]): Promise<void> {
-    await this.tryLoad(keys)
+    await this.tryLoad(keys, {
+      skipCache: false,
+      clearState: true
+    })
   }
 
   async refresh(keys: TKey[]): Promise<void> {
     const loadedKeys = keys.filter(key => {
       return this.pages.has(key)
     })
-    await this.tryLoad(loadedKeys, true)
+    await this.tryLoad(loadedKeys, {
+      skipCache: true,
+      clearState: false
+    })
   }
 }
